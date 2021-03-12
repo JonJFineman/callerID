c4e363ae5f17957aa3f819214a14b66395e518d6
jjf <jjf@desktop.jonjfineman.com>
2021-03-12 13:12:37 -0500

    update python release

--- start_callerid.sh
+++ start_callerid.sh
@@ -1,6 +1,7 @@
 set -x
 
-/usr/bin/mixerctl outputs.master=200,200
+#/usr/bin/mixerctl outputs.master=200,200
+/usr/bin/sndioctl output.level=1
 
 cd ~/src/callerID
-/usr/local/bin/python3 callBlock.py &
+/usr/local/bin/python3.8 callBlock.py &
