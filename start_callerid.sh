30c6cb4ecb64f0f45fe97d98b902b5bb50f5a7bb
Jon Fineman <jon@fineman.me>
2021-06-17 08:44:37 -0400

    add email notification

--- start_callerid.sh
+++ start_callerid.sh
@@ -4,4 +4,7 @@ set -x
 /usr/bin/sndioctl output.level=1
 
 cd ~/src/callerID
-/usr/local/bin/python3.8 callBlock.py &
+/bin/mv logs/callerID.log logs/callerID.log.txt
+/usr/bin/mail -s "call logs" jjf < logs/callerID.log.txt
+
+/usr/local/bin/python3.8 callBlock.py --email someid@someisp.com &
